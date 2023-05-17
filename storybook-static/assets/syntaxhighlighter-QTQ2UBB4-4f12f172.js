092cf918f6ee62ebdb1c7f1997c8dc38cd87d7c7
tuantk2 <tuantk2@fpt.com.vn>
2023-05-17 15:20:58 +0700

    x

---
+++ storybook-static/assets/syntaxhighlighter-QTQ2UBB4-4f12f172.js
@@ -0,0 +1,2 @@
+import{S as c,c as l,s as n}from"./DocsRenderer-EYKKDMVH-e6a398b5.js";import"./iframe-8efcde15.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-725317a4.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./index-671c6938.js";import"./index-356e4a49.js";export{c as SyntaxHighlighter,l as createCopyToClipboardFunction,n as default};
+//# sourceMappingURL=syntaxhighlighter-QTQ2UBB4-4f12f172.js.map
